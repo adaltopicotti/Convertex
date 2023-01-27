d564a555d06527e26f59e254bcd436b401916ebe
Adalto Picotti Junior <adaltocotti.jr@gmail.com>
2023-01-27 15:14:39 -0300

    Initial Commit

---
+++ Convertex/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Convertex
+//
+//  Created by Adalto Picotti Junior on 27/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Convertex/ConvertexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertexApp.swift
+//  Convertex
+//
+//  Created by Adalto Picotti Junior on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ConvertexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
